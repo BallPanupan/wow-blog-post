81f0e92f5e8cf775243adb582e17b12de578a8aa
Panupan <Ball085573@hotmail.com>
2024-08-12 01:49:31 +0700

    feat(page): /our Blog

---
+++ src/app/ourBlog/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import styles from "./ourBlog.module.css";
+import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import Navbar from "@/components/Navbar/Navbar";
+import MenuLeft from "@/components/MenuLeft/MenuLeft";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import Post from "@/components/PostList/Post";
+
+export default function OurBlog() {
+
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   router.push('/');
+  // }, [])
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="pt-5">
+
+        <div className="container-fluid">
+          <div className="row align-items-start justify-content-center">
+
+            <div className={`col-2 ${styles.offcanvasBody}`}><MenuLeft /></div>
+
+            <div className={`col-8 pt-5 mb-5 ${styles.mainContent}`}>
+              <SearchBar />
+
+              <div className={styles.listPosts}>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+              </div>
+
+            </div>
+
+            <div className={`col-2 ${styles.colRight}`}></div>
+
+
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  )
+
+}
